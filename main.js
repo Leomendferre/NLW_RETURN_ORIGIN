e3638cf7f2e2c2453e9d03b8edf8e453e107b3e8
Leonardo Mendonça Ferreira <leo.mend@hotmail.com>
2022-08-12 13:14:05 -0300

    Aula 3 finalizada.
    Estudo de HMTL, CSS, JS, pela empresa Rocketseat, com o evento NLW RETURN, na missão Origin.

---
+++ main.js
@@ -0,0 +1,31 @@
+function  onScroll() {
+  if(scrollY > 0) {
+    navigation.classList.add('scroll') 
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+
+function openMenu () {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu () {
+  document.body.classList.remove('menu-expanded')
+} 
+
+ScrollReveal({
+  origin:'top', // vai se movimentar do topo para baixo
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img,
+#home .stats,
+#services,
+#services header,
+#services card,
+#about,
+#about header,
+#about .content`)
